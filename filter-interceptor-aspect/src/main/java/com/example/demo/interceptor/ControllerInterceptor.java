79509965a347c93ca0ed928e4f11c6ed14a99be9
where.liu <where.liu@huolala.cn>
2021-09-24 15:24:24 +0800

    feat: save

--- filter-interceptor-aspect/src/main/java/com/example/demo/interceptor/ControllerInterceptor.java
+++ filter-interceptor-aspect/src/main/java/com/example/demo/interceptor/ControllerInterceptor.java
@@ -17,13 +17,13 @@ public class ControllerInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
         log.info("controllerInterceptor postHandle");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
         log.info("controllerInterceptor afterCompletion");
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
