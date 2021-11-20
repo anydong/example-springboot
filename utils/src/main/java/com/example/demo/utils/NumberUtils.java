8095c987e73a04667f20b9c131de00f82f4f6970
where.liu <where.liu@huolala.cn>
2021-11-20 16:59:34 +0800

    feat(NumberUtils): 新的 Number 方法

--- utils/src/main/java/com/example/demo/utils/NumberUtils.java
+++ utils/src/main/java/com/example/demo/utils/NumberUtils.java
@@ -1,35 +1,234 @@
 package com.example.demo.utils;
 
+
+/**
+ * Number 类型值校验
+ *
+ * @author where.liu
+ */
 public class NumberUtils {
-    public static Boolean isPositive(Integer number) {
-        return null != number && number > 0;
+    /**
+     * 校验 Integer 是否为 > 0 的正整数
+     *
+     * @param value 待校验的值，可以为 null
+     * @return true or false
+     */
+    public static boolean isPositive(Integer value) {
+        if (null == value) {
+            return false;
+        }
+        return value > 0;
     }
 
-    public static Boolean isPositive(Long number) {
-        return null != number && number > 0L;
+    /**
+     * 校验 Long 是否为 > 0 的正整数
+     *
+     * @param value 待校验的值，可以为 null
+     * @return true or false
+     */
+    public static boolean isPositive(Long value) {
+        if (null == value) {
+            return false;
+        }
+        return value > 0L;
     }
 
-    public static Boolean isPositiveOrZero(Integer number) {
-        return null != number && number >= 0;
+    /**
+     * 校验 Integer 是否为 >= 0 的正整数
+     *
+     * @param value 待校验的值，可以为 null
+     * @return true or false
+     */
+    public static boolean isPositiveOrZero(Integer value) {
+        if (null == value) {
+            return false;
+        }
+        return value >= 0;
     }
 
-    public static Boolean isPositiveOrZero(Long number) {
-        return null != number && number >= 0L;
+    /**
+     * 校验 Long 是否为 >= 0 的正整数
+     *
+     * @param value 待校验的值，可以为 null
+     * @return true or false
+     */
+    public static boolean isPositiveOrZero(Long value) {
+        if (null == value) {
+            return false;
+        }
+        return value >= 0L;
     }
 
-    public static Boolean isNegative(Integer number) {
-        return null != number && number < 0;
+    /**
+     * 校验 Integer 是否为 < 0 的整数
+     *
+     * @param value 待校验的值，可以为 null
+     * @return true or false
+     */
+    public static boolean isNegative(Integer value) {
+        if (null == value) {
+            return false;
+        }
+        return value < 0;
     }
 
-    public static Boolean isNegative(Long number) {
-        return null != number && number < 0L;
+    /**
+     * 校验 Long 是否为 < 0 的整数
+     *
+     * @param value 待校验的值，可以为 null
+     * @return true or false
+     */
+    public static boolean isNegative(Long value) {
+        if (null == value) {
+            return false;
+        }
+        return value < 0L;
     }
 
-    public static Boolean isNegativeOrZero(Integer number) {
-        return null != number && number <= 0;
+    /**
+     * 校验 Integer 是否为 <= 0 的整数
+     *
+     * @param value 待校验的值，可以为 null
+     * @return true or false
+     */
+    public static boolean isNegativeOrZero(Integer value) {
+        if (null == value) {
+            return false;
+        }
+        return value <= 0;
     }
 
-    public static Boolean isNegativeOrZero(Long number) {
-        return null != number && number <= 0L;
+    /**
+     * 校验 Long 是否为 <= 0 的整数
+     *
+     * @param value 待校验的值，可以为 null
+     * @return true or false
+     */
+    public static boolean isNegativeOrZero(Long value) {
+        if (null == value) {
+            return false;
+        }
+        return value <= 0L;
+    }
+
+    /**
+     * a < b
+     *
+     * @return true or false
+     */
+    public static boolean lt(Integer a, Integer b) {
+        if (null == a || null == b) {
+            return false;
+        }
+        return a.compareTo(b) < 0;
+    }
+
+    /**
+     * a < b
+     *
+     * @return true or false
+     */
+    public static boolean lt(Long a, Long b) {
+        if (null == a || null == b) {
+            return false;
+        }
+        return a.compareTo(b) < 0;
+    }
+
+    /**
+     * a <= b
+     *
+     * @return true or false
+     */
+    public static boolean le(Integer a, Integer b) {
+        if (null == a || null == b) {
+            return false;
+        }
+        return a.compareTo(b) <= 0;
+    }
+
+    /**
+     * a <= b
+     *
+     * @return true or false
+     */
+    public static boolean le(Long a, Long b) {
+        if (null == a || null == b) {
+            return false;
+        }
+        return a.compareTo(b) <= 0;
+    }
+
+    /**
+     * a == b
+     *
+     * @return true or false
+     */
+    public static boolean eq(Integer a, Integer b) {
+        if (null == a || null == b) {
+            return false;
+        }
+        return a.compareTo(b) == 0;
+    }
+
+    /**
+     * a == b
+     *
+     * @return true or false
+     */
+    public static boolean eq(Long a, Long b) {
+        if (null == a || null == b) {
+            return false;
+        }
+        return a.compareTo(b) == 0;
+    }
+
+    /**
+     * a > b
+     *
+     * @return true or false
+     */
+    public static boolean gt(Integer a, Integer b) {
+        if (null == a || null == b) {
+            return false;
+        }
+        return a.compareTo(b) > 0;
+    }
+
+    /**
+     * a > b
+     *
+     * @return true or false
+     */
+    public static boolean gt(Long a, Long b) {
+        if (null == a || null == b) {
+            return false;
+        }
+        return a.compareTo(b) > 0;
+    }
+
+    /**
+     * a >= b
+     *
+     * @return true or false
+     */
+    public static boolean ge(Integer a, Integer b) {
+        if (null == a || null == b) {
+            return false;
+        }
+        return a.compareTo(b) >= 0;
+    }
+
+    /**
+     * a >= b
+     *
+     * @return true or false
+     */
+    public static boolean ge(Long a, Long b) {
+        if (null == a || null == b) {
+            return false;
+        }
+        return a.compareTo(b) >= 0;
     }
 }
+
--- utils/src/test/java/com/example/demo/utils/NumberUtilsTests.java
+++ utils/src/test/java/com/example/demo/utils/NumberUtilsTests.java
@@ -1,56 +1,94 @@
 package com.example.demo.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
-@SpringBootTest
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class NumberUtilsTests {
-    @Test
-    public void isPositiveTest() {
-        // int
-        Assert.isTrue(NumberUtils.isPositive(1));
-        Assert.isTrue(!NumberUtils.isPositive(0));
-        Assert.isTrue(!NumberUtils.isPositive(-1));
-        // long
-        Assert.isTrue(NumberUtils.isPositive(1L));
-        Assert.isTrue(!NumberUtils.isPositive(0L));
-        Assert.isTrue(!NumberUtils.isPositive(-1L));
+
+    @ParameterizedTest
+    @MethodSource(value = "isPositiveIntegerParams")
+    public void isPositive(Integer value, boolean expect) {
+        boolean result = NumberUtils.isPositive(value);
+        Assertions.assertEquals(result, expect);
     }
 
-    @Test
-    public void isPositiveOrZeroTest() {
-        // int
-        Assert.isTrue(NumberUtils.isPositiveOrZero(1));
-        Assert.isTrue(NumberUtils.isPositiveOrZero(0));
-        Assert.isTrue(!NumberUtils.isPositiveOrZero(-1));
-        // long
-        Assert.isTrue(NumberUtils.isPositiveOrZero(1L));
-        Assert.isTrue(NumberUtils.isPositiveOrZero(0L));
-        Assert.isTrue(!NumberUtils.isPositiveOrZero(-1L));
+    public static List<Arguments> isPositiveIntegerParams() {
+        return Arrays.asList(
+                Arguments.arguments(null, false),
+                Arguments.arguments(0, false),
+                Arguments.arguments(-1, false),
+                Arguments.arguments(1, true)
+        );
     }
 
-    @Test
-    public void isNegativeTest() {
-        // int
-        Assert.isTrue(NumberUtils.isNegative(-1));
-        Assert.isTrue(!NumberUtils.isNegative(1));
-        Assert.isTrue(!NumberUtils.isNegative(0));
-        // long
-        Assert.isTrue(NumberUtils.isNegative(-1L));
-        Assert.isTrue(!NumberUtils.isNegative(1L));
-        Assert.isTrue(!NumberUtils.isNegative(0L));
+    @ParameterizedTest
+    @MethodSource(value = "isPositiveLongParams")
+    public void isPositive(Long value, boolean expect) {
+        boolean result = NumberUtils.isPositive(value);
+        Assertions.assertEquals(result, expect);
     }
 
-    @Test
-    public void isNegativeOrZeroTest() {
-        // int
-        Assert.isTrue(NumberUtils.isNegativeOrZero(0));
-        Assert.isTrue(NumberUtils.isNegativeOrZero(-1));
-        Assert.isTrue(!NumberUtils.isNegativeOrZero(1));
-        // long
-        Assert.isTrue(NumberUtils.isNegativeOrZero(0L));
-        Assert.isTrue(NumberUtils.isNegativeOrZero(-1L));
-        Assert.isTrue(!NumberUtils.isNegativeOrZero(1L));
+    public static List<Arguments> isPositiveLongParams() {
+        return Arrays.asList(
+                Arguments.arguments(null, false),
+                Arguments.arguments(0L, false),
+                Arguments.arguments(-1L, false),
+                Arguments.arguments(1L, true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "isPositiveOrZeroIntegerParams")
+    public void isPositiveOrZero(Integer value, boolean expect) {
+        boolean result = NumberUtils.isPositiveOrZero(value);
+        Assertions.assertEquals(result, expect);
+    }
+
+    public static List<Arguments> isPositiveOrZeroIntegerParams() {
+        return Arrays.asList(
+                Arguments.arguments(null, false),
+                Arguments.arguments(-1, false),
+                Arguments.arguments(0, true),
+                Arguments.arguments(1, true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "isNegativeIntegerParams")
+    public void isNegative(Integer value, boolean expect) {
+        boolean result = NumberUtils.isNegative(value);
+        Assertions.assertEquals(result, expect);
+    }
+
+    public static List<Arguments> isNegativeIntegerParams() {
+        return Arrays.asList(
+                Arguments.arguments(null, false),
+                Arguments.arguments(0, false),
+                Arguments.arguments(1, false),
+                Arguments.arguments(-1, true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "isNegativeOrZeroIntegerParams")
+    public void isNegativeOrZero(Integer value, boolean expect) {
+        boolean result = NumberUtils.isNegativeOrZero(value);
+        Assertions.assertEquals(result, expect);
+    }
+
+    public static List<Arguments> isNegativeOrZeroIntegerParams() {
+        return Arrays.asList(
+                Arguments.arguments(null, false),
+                Arguments.arguments(1, false),
+                Arguments.arguments(0, true),
+                Arguments.arguments(-1, true)
+        );
     }
 }
