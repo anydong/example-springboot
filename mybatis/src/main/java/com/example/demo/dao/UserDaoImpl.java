6857532c21809a2e0271ebe764fc4246dc086f5f
where.liu <where.liu@huolala.cn>
2021-12-08 09:52:05 +0800

    feat: inject

--- mybatis/src/main/java/com/example/demo/dao/UserDaoImpl.java
+++ mybatis/src/main/java/com/example/demo/dao/UserDaoImpl.java
@@ -3,6 +3,11 @@ package com.example.demo.dao;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.demo.dataobject.UserDO;
 import com.example.demo.mapper.UserMapper;
+import org.springframework.stereotype.Component;
 
+/**
+ * @author where
+ */
+@Component
 public class UserDaoImpl extends ServiceImpl<UserMapper, UserDO> implements BaseDaoI<UserDO> {
 }
