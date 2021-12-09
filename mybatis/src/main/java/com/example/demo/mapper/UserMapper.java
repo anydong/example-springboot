68fbc869a218eefc64cf6caee0432387658cd6f8
where.liu <where.liu@huolala.cn>
2021-12-09 10:34:34 +0800

    feat: save

--- mybatis/src/main/java/com/example/demo/mapper/UserMapper.java
+++ mybatis/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -4,6 +4,16 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.dataobject.UserDO;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author where
+ */
 @Component
 public interface UserMapper extends BaseMapper<UserDO> {
+    /**
+     * 根据 ID 查询
+     *
+     * @param id ID
+     * @return nullable
+     */
+    UserDO find(Long id);
 }
