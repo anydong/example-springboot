809e0c9538c52917f7d0692d4c48e8db569e100e
where.liu <where.liu@huolala.cn>
2021-12-10 17:56:33 +0800

    feat(pom): update

--- mybatis/src/main/java/com/example/demo/dao/UserDaoImpl.java
+++ mybatis/src/main/java/com/example/demo/dao/UserDaoImpl.java
@@ -1,8 +1,12 @@
 package com.example.demo.dao;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.demo.dataobject.UserDO;
 import com.example.demo.mapper.UserMapper;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,4 +14,18 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class UserDaoImpl extends ServiceImpl<UserMapper, UserDO> implements BaseDaoI<UserDO> {
+    public UserDO getLastOne() {
+        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery();
+        queryWrapper.orderByDesc(UserDO::getId);
+        queryWrapper.last("LIMIT 1");
+        return getOne(queryWrapper);
+    }
+
+    public UserDO createRandomOne() {
+        UserDO userDO = new UserDO();
+        userDO.setUsername(RandomStringUtils.random(8));
+        userDO.setPassword(RandomStringUtils.random(16));
+        save(userDO);
+        return userDO;
+    }
 }
--- mybatis/src/test/java/com/example/demo/mybatis/MybatisPlusTests.java
+++ mybatis/src/test/java/com/example/demo/mybatis/MybatisPlusTests.java
@@ -1,8 +1,12 @@
 package com.example.demo.mybatis;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.example.demo.dao.UserDaoImpl;
 import com.example.demo.dataobject.UserDO;
 import com.example.demo.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +18,8 @@ import java.util.List;
 public class MybatisPlusTests {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private UserDaoImpl userDao;
 
     @Test
     public void mapper() {
@@ -23,5 +29,40 @@ public class MybatisPlusTests {
         userMapper.insert(userDO);
         List<UserDO> userDOList = userMapper.selectList(null);
         log.info("{}", userDOList);
+    }
+
+    /**
+     * 乐观锁只在 updateById 这种场景会生效
+     */
+    @Test
+    public void entityUpdate() {
+        UserDO userDO = userDao.getLastOne();
+        if (null == userDO) {
+            userDO = userDao.createRandomOne();
+        }
+        userDO.setPassword(RandomStringUtils.random(6));
+        userMapper.updateById(userDO);
+    }
+
+    /**
+     * 纯 UpdateWrapper 更新不会更新乐观锁
+     */
+    @Test
+    public void mapperUpdate() {
+        UserDO userDO = userDao.createRandomOne();
+        LambdaUpdateWrapper<UserDO> updateWrapper = Wrappers.lambdaUpdate();
+        updateWrapper.eq(UserDO::getId, userDO.getId());
+        updateWrapper.set(UserDO::getPassword, RandomStringUtils.randomAlphabetic(6));
+        userMapper.update(null, updateWrapper);
+    }
+
+    @Test
+    public void entityAndMapperUpdate() {
+        UserDO userDO = userDao.createRandomOne();
+        LambdaUpdateWrapper<UserDO> updateWrapper = Wrappers.lambdaUpdate();
+        updateWrapper.eq(UserDO::getId, userDO.getId());
+        UserDO updateEntity = new UserDO();
+        updateEntity.setPassword(RandomStringUtils.randomAlphabetic(6));
+        userMapper.update(updateEntity, updateWrapper);
     }
 }
