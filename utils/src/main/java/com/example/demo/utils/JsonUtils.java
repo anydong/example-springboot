706b930e93cc937564b7898d3c5e715b456ce3db
where.liu <where.liu@huolala.cn>
2021-12-20 15:23:55 +0800

    feat(JsonUtils): 添加单元测试

---
+++ utils/src/main/java/com/example/demo/utils/JsonUtils.java
@@ -0,0 +1,65 @@
+package com.example.demo.utils;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 仿照 FastJSON 的接口，生成 Jackson 的静态调用方法
+ *
+ * @author where
+ */
+@Slf4j
+public class JsonUtils {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        MAPPER.registerModule(new JavaTimeModule());
+        MAPPER.registerModule(new SimpleModule());
+        // https://github.com/FasterXML/jackson-databind/wiki/Serialization-Features#:~:text=Error%20handling-,FAIL_ON_EMPTY_BEANS,-(default%3A%20true)
+        MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        // https://github.com/FasterXML/jackson-databind/wiki/Serialization-Features#:~:text=Generic%20output%20features-,WRAP_ROOT_VALUE,-(default%3A%20false)
+        MAPPER.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        // https://github.com/FasterXML/jackson-databind/wiki/Serialization-Features#:~:text=WRITE_DATES_AS_TIMESTAMPS
+        MAPPER.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
+        // https://github.com/FasterXML/jackson-databind/wiki/Deserialization-Features#:~:text=FAIL_ON_UNKNOWN_PROPERTIES
+        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        MAPPER.configure(MapperFeature.USE_ANNOTATIONS, true);
+        MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
+    public static String toJSONString(Object object) {
+        try {
+            return MAPPER.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            log.error("toJSONString error", e);
+        }
+        return null;
+    }
+
+    public static <T> T parseObject(String json, Class<T> clazz) {
+        try {
+            return MAPPER.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            log.error("parseObject error", e);
+        }
+        return null;
+    }
+
+    public static <T> List<T> parseArray(String json, Class<T> clazz) {
+        try {
+            JavaType type = MAPPER.getTypeFactory().constructParametricType(ArrayList.class, clazz);
+            return MAPPER.readValue(json, type);
+        } catch (JsonProcessingException e) {
+            log.error("parseArray error", e);
+        }
+        return null;
+    }
+}
---
+++ utils/src/test/java/com/example/demo/utils/JsonUtilsTests.java
@@ -0,0 +1,54 @@
+package com.example.demo.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Slf4j
+public class JsonUtilsTests {
+    @Test
+    public void toJSONString() {
+        User user = new User();
+        user.setUsername("username");
+        String json = JsonUtils.toJSONString(user);
+        log.info(json);
+        Assertions.assertNotNull(json);
+    }
+
+    @Test
+    public void parseObject() {
+        String json = "{\"username\":\"username\",\"password\":null}";
+        User user = JsonUtils.parseObject(json, User.class);
+        log.info("username {}", user.getUsername());
+        Assertions.assertNotNull(user);
+    }
+
+    @Test
+    public void parseArray() {
+        List<User> users = new ArrayList<>();
+        users.add(User.builder().username("username").build());
+        String json = JsonUtils.toJSONString(users);
+        log.info(json);
+        List<User> parseResult = JsonUtils.parseArray(json, User.class);
+        log.info(parseResult.get(0).getUsername());
+        Assertions.assertEquals(1, parseResult.size());
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class User {
+        private String username;
+        private String password;
+    }
+}
