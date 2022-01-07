0e80479ddc745782ff8b02d7cbee98c963e1acb8
where.liu <where.liu@huolala.cn>
2022-01-07 09:25:30 +0800

    feat(JsonUtils): 添加注释

--- utils/src/test/java/com/example/demo/annotation/validator/InTests.java
+++ utils/src/test/java/com/example/demo/annotation/validator/InTests.java
@@ -1,15 +1,15 @@
 package com.example.demo.annotation.validator;
 
-import com.example.demo.query.RegisterQuery;
+import com.example.demo.utils.JsonUtils;
 import com.example.demo.utils.ValidatorUtils;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
@@ -20,21 +20,36 @@ import java.util.stream.Stream;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Slf4j
 public class InTests {
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @ParameterizedTest
     @MethodSource("inTest")
-    public void inTest(RegisterQuery query, int size) throws JsonProcessingException {
-        Map<String, List<String>> errors = ValidatorUtils.validate(query);
-        log.info(objectMapper.writeValueAsString(errors));
+    public void inTest(Tester tester, int size) {
+        Map<String, List<String>> errors = ValidatorUtils.validate(tester);
+        log.info(JsonUtils.toJSONString(errors));
         Assert.isTrue(errors.size() == size, "结果不一致");
     }
 
     private static Stream<Arguments> inTest() {
         return Stream.of(
-                Arguments.arguments(RegisterQuery.builder().sex(1).build(), 0),
-                Arguments.arguments(RegisterQuery.builder().sex(3).build(), 1)
+                Arguments.arguments(Tester.builder().string("a").build(), 0),
+                Arguments.arguments(Tester.builder().string("c").build(), 1),
+                Arguments.arguments(Tester.builder().integer(1).build(), 0),
+                Arguments.arguments(Tester.builder().integer(3).build(), 1),
+                Arguments.arguments(Tester.builder().bool(true).build(), 0),
+                Arguments.arguments(Tester.builder().bool(null).build(), 0)
         );
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class Tester {
+        @In(allows = {"a", "b"})
+        private String string;
+        @In(allows = {"1", "2"})
+        private Integer integer;
+        @In(allows = {"true", "false"})
+        private Boolean bool;
     }
 }
