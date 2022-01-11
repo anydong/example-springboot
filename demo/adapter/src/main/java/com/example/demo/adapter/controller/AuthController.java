5f55b36d545a41f709c2653a95d48a2d9fb75079
where.liu <where.liu@huolala.cn>
2022-01-11 20:01:12 +0800

    feat: save

--- demo/adapter/src/main/java/com/example/demo/adapter/controller/AuthController.java
+++ demo/adapter/src/main/java/com/example/demo/adapter/controller/AuthController.java
@@ -2,10 +2,9 @@ package com.example.demo.adapter.controller;
 
 import com.alibaba.cola.dto.Response;
 import com.alibaba.cola.dto.SingleResponse;
+import com.example.demo.adapter.logic.AuthLogic;
 import com.example.demo.adapter.model.request.UsernameLoginCmd;
 import com.example.demo.adapter.model.request.UsernameRegisterCmd;
-import com.example.demo.domain.auth.request.UserRegisterCmd;
-import com.example.demo.adapter.logic.AuthLogic;
 import com.example.demo.adapter.model.response.LoginTokenVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
