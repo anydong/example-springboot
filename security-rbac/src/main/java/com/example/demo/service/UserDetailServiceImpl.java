bc69a06a80919d4509850e43b9be2639f241d3c6
where.liu <where.liu@huolala.cn>
2022-05-18 12:28:40 +0800

    feat: security

--- security-rbac/src/main/java/com/example/demo/service/UserDetailServiceImpl.java
+++ security-rbac/src/main/java/com/example/demo/service/UserDetailServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,9 +11,11 @@ import org.springframework.stereotype.Component;
  * @since 2022/5/17
  */
 @Component
+@Slf4j
 public class UserDetailServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("username {}", username);
         return null;
     }
 }
