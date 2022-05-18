dde9c392b988e6d582a44e61e1e4dff8d0c27de4
where.liu <where.liu@huolala.cn>
2022-05-18 13:52:23 +0800

    feat: security

--- security-rbac/src/main/java/com/example/demo/security/LoginAuthenticationFailureHandler.java
+++ security-rbac/src/main/java/com/example/demo/security/LoginAuthenticationFailureHandler.java
@@ -1,6 +1,9 @@
 package com.example.demo.security;
 
+import com.alibaba.cola.dto.Response;
+import com.example.demo.util.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -9,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Where.LIU
@@ -19,6 +23,12 @@ import java.io.IOException;
 public class LoginAuthenticationFailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        log.info("login failure");
+        String content = JSONUtil.toJSONString(Response.buildFailure("A0403", exception.getMessage()));
+        if (null == content) {
+            content = "";
+        }
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.getWriter().write(content);
     }
 }
