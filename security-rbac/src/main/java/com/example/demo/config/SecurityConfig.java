b6aa5152296c00e6bf80a37981a6e092d09e44db
where.liu <where.liu@huolala.cn>
2022-05-18 12:32:21 +0800

    feat: security

--- security-rbac/src/main/java/com/example/demo/config/SecurityConfig.java
+++ security-rbac/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,20 +1,14 @@
 package com.example.demo.config;
 
-import com.example.demo.security.*;
-import com.example.demo.service.UserDetailServiceImpl;
+import com.example.demo.security.LoginAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -56,17 +50,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return filter;
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.sessionManagement().disable();
         http.csrf().disable();
         http.addFilterBefore(loginAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.authorizeRequests().anyRequest().authenticated();
         http.authorizeRequests().antMatchers("/index").permitAll();
+        http.authorizeRequests().anyRequest().authenticated();
+
     }
 }
